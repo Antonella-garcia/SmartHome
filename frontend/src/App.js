bf0f3ba0979fd054e4f4665ae6d0e352fec933ab
“Antonella-garcia” <cantgarc@espol.edu.ec>
2021-07-11 12:39:26 -0500

    cambios front, foto, texto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Navigation from './components/Navigation'
 import React from 'react'
-import home from './Images/home.png' ;
+import home from './Images/home.png';
 import { Route, HashRouter } from 'react-router-dom'
 
 function App() {
@@ -12,9 +12,15 @@ function App() {
       </div>
 
       <Route exact path="/" render={() => (
-        <img src={home} className="img-thumbnail" alt="Home" />
-
-
+        <div id="home">
+          <div classname="landing-text"  display="table-cell" text-align="center" vertical-align="middle" padding="80px 0">
+            <h1  font-size="500%" font-weight="700" display="table-cell" text-align="center" vertical-align="middle" padding="80px 0">SMARTHOME</h1>
+            <h3 display="table-cell" text-align="center" vertical-align="middle" padding="80px 0">Bienvenido, escoge una de nuestras opciones</h3>
+          </div>
+          <div className="img-home">
+            <img src={home} className="rounded mx-auto d-block" alt="Home" width="100%" display="table" height="100%" position="center" background-size="cover" />
+          </div>
+        </div>
       )} />
     </HashRouter>
   );
